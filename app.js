2cbfd4a33ead2f36a1ea83bb0491d65f93ab4de0
max_ivaneychyk <maxym2010i@gmail.com>
2017-10-03 15:54:58 +0300

    fill in gitignore

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let bodyParser = require("body-parser");
+let diskinfo = require('diskinfo');
+let fsExtra = require('fs-extra');
 let fs = require('fs');
+
 // подключение express
 let express = require("express");
 
@@ -24,14 +27,95 @@ ee.on('get-files-from-dir', function (data) {
 
 ee.on('get-disks', function (data) {
 // GET DISK
-    const DISK_INFO = 'wmic logicaldisk get name';
-    cmd(DISK_INFO, function (err, stdout) {
-        if (err) throw err;
-        // console.log(stdout);
-        let disks = String(stdout).replace(/\s+/g, ' ').trim().split(' ').splice(1);
+    diskinfo.getDrives(function(err, aDrives) {
+        let disks = [];
+        for (var i = 0; i < aDrives.length; i++) {
+            // console.log('Drive ' + aDrives[i].filesystem);
+            // console.log('blocks ' + aDrives[i].blocks);
+            // console.log('used ' + aDrives[i].used);
+            // console.log('available ' + aDrives[i].available);
+            // console.log('capacity ' + aDrives[i].capacity);
+            // console.log('mounted ' + aDrives[i].mounted);
+            disks.push(aDrives[i].mounted);
+            // console.log('-----------------------------------------');
+        }
+
         data.callback(disks);
     });
 });
+
+// СОЗДАТЬ ДИРЕКТОРИЮ
+ee.on('create-folder', function (data) {
+    let dir = data.dir;
+
+    fsExtra.ensureDir(dir)
+        .then(() => {
+            data.callback('success');
+        })
+        .catch(err => {
+            data.callback('error', err);
+        });
+});
+//ee.emit('create-folder', {dir: 'C:\\\\test.txt'});
+
+// ПРОВЕРИТЬ ПУТЬ НА СУЩЕСТВОВАНИЕ
+ee.on('path-exists', function (data) {
+    // Promise usage:
+    fsExtra.pathExists(data.dir)
+        .then(exists => {
+            data.callback(exists);
+        }) // => false
+        .catch(err => {
+            data.callback('error', err);
+        });
+});
+/*ee.emit('path-exists', {dir: 'C:\\\\test.tx', callback: function (status) {
+    console.log(status);
+}});*/
+
+
+
+
+ee.on('ensure-dir', function (data) {
+    let dir = data.dir;
+// With Promises:
+    fsExtra.ensureDir(dir)
+        .then(() => {
+            data.callback(true)
+        })
+        .catch(err => {
+            data.callback(false, err)
+        })
+});
+
+
+ee.on('rename-file', function (data) {
+    const COMMAND = 'rename c:\\new_folder renamed_folder';
+    cmd(COMMAND, function (err, stdout) {
+        if (err) throw err;
+        // console.log(stdout);
+        data.callback();
+    });
+});
+
+// todo cool
+ee.on('delete-file', function (data) {
+    ;['D://node/node-explorer/del.txt'].forEach(function(filename) {
+        fs.unlink(filename);
+    });
+    data.callback();
+});
+
+ee.on('delete-folder', function (data) {
+    const COMMAND = 'rd d:\\test /S /Q';
+    cmd(COMMAND, function (err, stdout) {
+        if (err) throw err;
+        // console.log(stdout);
+        data.callback();
+    });
+});
+
+
 
 
 app.post('/path/', urlencodedParser, function (req, res) {
@@ -66,5 +150,6 @@ app.get("/", function (request, response) {
 
 app.use(express.static(__dirname + "/public"));
 // начинаем прослушивать подключения на 3000 порту
-app.listen(3000);
+//app.listen(3000);
+
 
