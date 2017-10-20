acd720f1842f2dfb0848f49753be68bf8ede3143
max_ivaneychyk <maxym2010i@gmail.com>
2017-10-20 13:09:33 +0300

    refactor code

--- server.js
+++ server.js
@@ -4,20 +4,14 @@ let diskinfo = require("diskinfo");
 let fsExtra = require("fs-extra");
 let path = require("path");
 let fs = require("fs");
-
-const PORT = 3000;
-
 // подключение express
 let express = require("express");
-
-let Emitter = require("events");
-let ee = new Emitter();
-
 // создаем объект приложения
 let app = express();
-//app.use(express.methodOverride()); // поддержка put и delete
 // создаем парсер для данных application/x-www-form-urlencoded
 let urlencodedParser = bodyParser.urlencoded({extended: false});
+
+const PORT = 3000;
 
 ///////////////     DISKS   INFO
 let disks = [];
@@ -25,147 +19,6 @@ diskinfo.getDrives(function (err, aDrives) {
     disks = aDrives.map((disk) => {
         disk.format = 'drive';
         return disk;
-    });
-});
-
-/*log.info('GET-FILES-DISKS ');
-log.debug('GET-FILES-DISKS ');
-log.error('GET-FILES-DISKS ');
-log.info('GET-FILES-DISKS ');*/
-
-ee.on("get-disks", function (data) {
-    // GET DISK
-    console.log("GET-FILES-DISKS ");
-    data.callback(disks);
-});
-
-ee.on("get-files-from-dir", function (data) {
-    // кодирование плюсов
-    let currentDirPath = data.path.replace(/\$plus/gim, "+");
-    let list = [];
-
-    fs.readdir(currentDirPath, function (err, files) {
-        if (err) {
-            console.log(err);
-            data.callback({error: err});
-            return;
-        }
-
-        files.forEach(function (name) {
-            // .SYS  не обрабативаем, нет прав
-            var filePath = path.join(currentDirPath, name);
-            var stat;
-            try {
-                stat = fs.statSync(filePath);
-            } catch (errFile) {
-                //console.log('Cant read system file ', errFile);
-                return;
-            }
-
-            if (stat.isFile()) {
-                list.push({name: name, stat: stat, format: "file"});
-            } else if (stat.isDirectory()) {
-                list.push({name: name, stat: stat, format: "directory"});
-            }
-        });
-        console.log("GET-FILES-FROM ", currentDirPath);
-        // send data to client
-        data.callback(list);
-    });
-});
-
-// СОЗДАТЬ ДИРЕКТОРИЮ
-ee.on("create-folder", function (data) {
-    let dir = data.path;
-
-    fsExtra
-        .ensureDir(dir)
-        .then(() => {
-            console.log("CREATE-NEW-FOLDER ", dir);
-            data.callback("success");
-        })
-        .catch(err => {
-            data.callback("error", err);
-        });
-});
-//ee.emit('create-folder', {dir: 'C:\\\\test.txt'});
-
-// ПРОВЕРИТЬ ПУТЬ НА СУЩЕСТВОВАНИЕ
-ee.on("path-exists", function (data) {
-    // Promise usage:
-    fsExtra
-        .pathExists(data.dir)
-        .then(exists => {
-            data.callback(exists);
-        }) // => false
-        .catch(err => {
-            data.callback("error", err);
-        });
-});
-
-ee.on("rename-file", function (data) {
-    fs.rename(data.path, data.newPath, err => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("RENAME-FILE ", data.path, " TO ", data.newPath);
-        data.callback(true);
-    });
-});
-
-ee.on("delete", function (data) {
-    let files = JSON.parse(data.files || "[]");
-    files.forEach(function (filename) {
-        console.log("DELETE-FILE ", filename);
-        fsExtra.removeSync(filename);
-    });
-    data.callback();
-});
-
-app.post("/ls/", urlencodedParser, function (req, res) {
-    if (!req.body) return res.sendStatus(400);
-    let url = req.body.url;
-    if (!url) {
-        url = "drivers";
-    }
-    let eventName = url === "drivers" ? "get-disks" : "get-files-from-dir";
-
-    ee.emit(eventName, {
-        path: url,
-        callback: function (list) {
-            if (url === "drivers") {
-                list.forEach(data => (data.format = "drive"));
-            }
-
-            list = JSON.stringify(list);
-            res.setHeader("Content-Type", "application/json");
-            res.end(list);
-        }
-    });
-});
-
-app.post("/command/", urlencodedParser, function (req, res) {
-    const events = ["create-folder", "rename-file", "delete"];
-
-    if (!req.body) return res.sendStatus(400);
-
-    let eventName = req.body.event;
-    if (events.indexOf(eventName) === -1) {
-        let data = JSON.stringify({status: "error", desc: "Bad code in command"});
-        res.setHeader("Content-Type", "application/json");
-        res.end(data);
-        return;
-    }
-
-    ee.emit(eventName, {
-        path: req.body.path,
-        newPath: req.body.newPath,
-        files: req.body.files,
-        callback: function (status) {
-            let data = JSON.stringify({status: status});
-            res.setHeader("Content-Type", "application/json");
-            res.end(data);
-        }
     });
 });
 
@@ -233,29 +86,31 @@ app.get("/files", function (req, res) {
 
         getDriversInfo()
             .then((list) => {
-
                 res.json(list);
-            }).catch((err) => {
+            })
+            .catch((err) => {
 
-            let message = "Not found path ";
-            res.statusCode = 404;
-            res.send(message);
+                let message = "Not found path ";
+                res.statusCode = 404;
+                res.send(message);
 
-            log.error(message + pathExplorer, err);
-        });
+                log.error(message + pathExplorer, err);
+            });
 
     } else {
 
-        getFilesInfo(pathExplorer).then((list) => {
-            res.json(list);
-        }).catch(() => {
+        getFilesInfo(pathExplorer)
+            .then((list) => {
+                res.json(list);
+            })
+            .catch(() => {
 
-            let message = "Not found path ";
-            res.statusCode = 404;
-            res.send(message);
+                let message = "Not found path ";
+                res.statusCode = 404;
+                res.send(message);
 
-            log.error(message + pathExplorer, err);
-        });
+                log.error(message + pathExplorer, err);
+            });
 
     }
 });
@@ -295,10 +150,10 @@ app.put('/file', urlencodedParser, function (req, res) {
     fsExtra.rename(path, newPath)
         .then(() => {
             res.json({path, newPath})
-        }).
-    catch((err) => {
-        res.json({err})
-    });
+        })
+        .catch((err) => {
+            res.json({err})
+        });
 });
 
 app.use(express.static(__dirname + "/public"));
